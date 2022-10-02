7af798747740dc21ce6b05dd4962c376a1ef4e82
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2022-10-02 01:53:17 -0700

    working same scalar

--- curdleproofs/commitment.py
+++ curdleproofs/commitment.py
@@ -1,8 +1,8 @@
-from audioop import mul
+import random
 from typing import TypeVar
-from curdleproofs.crs import CurdleproofsCrs
+from crs import CurdleproofsCrs, get_random_point
 from util import PointAffine, PointProjective, Fr, field_to_bytes
-from py_ecc.optimized_bls12_381.optimized_curve import curve_order, G1, multiply, normalize, add, Z1
+from py_ecc.optimized_bls12_381.optimized_curve import curve_order, G1, multiply, normalize, add, Z1, eq
 
 T_GroupCommitment = TypeVar('T_GroupCommitment', bound="GroupCommitment")
 
@@ -21,10 +21,34 @@ class GroupCommitment:
     T: PointProjective,
     r: Fr
   ) -> None:
-    return cls(multiply(crs_G, r), add(T, multiply(crs_H, r)))
+    return cls(multiply(crs_G, int(r)), add(T, multiply(crs_H, int(r))))
 
   def __add__(self, other: T_GroupCommitment) -> T_GroupCommitment:
     return type(self)(add(self.T_1, other.T_1), add(self.T_2, other.T_2))
   
   def __mul__(self, other: Fr) -> T_GroupCommitment:
-    return type(self)(multiply(self.T_1, other), multiply(self.T_2, other))
+    return type(self)(multiply(self.T_1, int(other)), multiply(self.T_2, int(other)))
+
+  def __eq__(self, __o: object) -> bool:
+    if not isinstance(__o, GroupCommitment):
+      return NotImplemented
+    return eq(self.T_1, __o.T_1) and eq(self.T_2, __o.T_2)
+
+
+def test_group_commit():
+  crs_G = get_random_point()
+  crs_H = get_random_point()
+
+  A = get_random_point()
+  B = get_random_point()
+
+  r_a = Fr(random.randint(1, Fr.field_modulus))
+  r_b = Fr(random.randint(1, Fr.field_modulus))
+
+  cm_a = GroupCommitment.new(crs_G, crs_H, A, r_a)
+  cm_b = GroupCommitment.new(crs_G, crs_H, B, r_b)
+  cm_a_b = GroupCommitment.new(crs_G, crs_H, add(A, B), r_a + r_b)
+
+  assert cm_a + cm_b == cm_a_b
+
+# test_group_commit()
--- curdleproofs/same_scalar.py
+++ curdleproofs/same_scalar.py
@@ -1,17 +1,17 @@
 import itertools
+import random
 
-from crs import CurdleproofsCrs
-from curdleproofs.commitment import GroupCommitment
-from grand_prod import grand_product_verify
-from ipa import ipa_verify
-from util import points_to_bytes, point_to_bytes
+from crs import CurdleproofsCrs, get_random_point
+from commitment import GroupCommitment
+from util import points_projective_to_bytes
 from transcript import CurdleproofsTranscript
-from typing import List, Tuple
+from typing import List, Tuple, TypeVar
 from util import PointAffine, PointProjective, Fr, field_to_bytes
 from msm_accumulator import MSMAccumulatorInefficient
 from py_ecc.optimized_bls12_381.optimized_curve import curve_order, G1, multiply, normalize, add, neg
 from operator import mul as op_mul
 
+T_SameScalarProof = TypeVar('T_SameScalarProof', bound="SameScalarProof")
 class SameScalarProof:
   def __init__(self, cm_A: GroupCommitment, cm_B: GroupCommitment, z_k: Fr, z_t: Fr, z_u: Fr) -> None:
     self.cm_A = cm_A
@@ -20,23 +20,107 @@ class SameScalarProof:
     self.z_t = z_t
     self.z_u = z_u
 
-  def verify(self,
-    crs: CurdleproofsCrs,
+  @classmethod
+  def new(cls,
+    crs_G_t: PointProjective,
+    crs_G_u: PointProjective,
+    crs_H: PointProjective,
+
     R: PointProjective,
     S: PointProjective,
     cm_T: GroupCommitment,
     cm_U: GroupCommitment,
-    transcript: CurdleproofsTranscript) -> Tuple[bool, str]:
-    transcript.append_list(b'sameexp_points', [R, S, cm_T.T_1, cm_T.T_2, cm_U.T_1, cm_U.T_2, self.cm_A.T_1, self.cm_A.T_2, self.cm_B.T_1, self.cm_B.T_2].map(points_to_bytes))
+    k: Fr,
+    r_t: Fr,
+    r_u: Fr,
+    transcript: CurdleproofsTranscript
+  ) -> T_SameScalarProof:
+    r_a = Fr(random.randint(1, Fr.field_modulus))
+    r_b = Fr(random.randint(1, Fr.field_modulus))
+    r_k = Fr(random.randint(1, Fr.field_modulus))
+
+    cm_A = GroupCommitment.new(crs_G_t, crs_H, multiply(R, int(r_k)), r_a)
+    cm_B = GroupCommitment.new(crs_G_u, crs_H, multiply(S, int(r_k)), r_b)
+
+    transcript.append_list(b'sameexp_points', points_projective_to_bytes([R, S, cm_T.T_1, cm_T.T_2, cm_U.T_1, cm_U.T_2, cm_A.T_1, cm_A.T_2, cm_B.T_1, cm_B.T_2]))
+    alpha = transcript.get_and_append_challenge(b'same_scalar_alpha')
+
+    z_k = r_k + k * alpha
+    z_t = r_a + r_t * alpha
+    z_u = r_b + r_u * alpha
+
+    return cls(cm_A, cm_B, z_k, z_t, z_u)
+
+  def verify(self,
+    crs_G_t: PointProjective,
+    crs_G_u: PointProjective,
+    crs_H: PointProjective,
+
+    R: PointProjective,
+    S: PointProjective,
+    cm_T: GroupCommitment,
+    cm_U: GroupCommitment,
+    transcript: CurdleproofsTranscript
+  ) -> Tuple[bool, str]:
+    transcript.append_list(b'sameexp_points', points_projective_to_bytes([R, S, cm_T.T_1, cm_T.T_2, cm_U.T_1, cm_U.T_2, self.cm_A.T_1, self.cm_A.T_2, self.cm_B.T_1, self.cm_B.T_2]))
 
     alpha = transcript.get_and_append_challenge(b'same_scalar_alpha');
-    expected_1 = GroupCommitment(crs.G_t, crs.H, multiply(R, int(self.z_k)), self.z_t);
-    expected_2 = GroupCommitment(crs.G_t, crs.H, multiply(S, int(self.z_k)), self.z_u);
+    expected_1 = GroupCommitment.new(crs_G_t, crs_H, multiply(R, int(self.z_k)), self.z_t);
+    expected_2 = GroupCommitment.new(crs_G_u, crs_H, multiply(S, int(self.z_k)), self.z_u);
 
-    computed_1 = self.cm_A + cm_T * alpha
-    computed_2 = self.cm_B + cm_U * alpha
+    computed_1 = self.cm_A + (cm_T * alpha)
+    computed_2 = self.cm_B + (cm_U * alpha)
 
-    if expected_1.T_1 == computed_1.T_1 and expected_1.T_2 == computed_1.T_2 and expected_2.T_1 == computed_2.T_1 and expected_2.T_2 == computed_2.T_2:
+    if expected_1 == computed_1 and expected_2 == computed_2:
       return (True, "")
     else:
       return (False, "Failure")
+
+def test_same_scalar_arg():
+  transcript_prover = CurdleproofsTranscript()
+
+  crs_G_t = get_random_point()
+  crs_G_u = get_random_point()
+  crs_H = get_random_point()
+
+  R = get_random_point()
+  S = get_random_point()
+
+  k = Fr(random.randint(1, Fr.field_modulus))
+  r_t = Fr(random.randint(1, Fr.field_modulus))
+  r_u = Fr(random.randint(1, Fr.field_modulus))
+
+  cm_T = GroupCommitment.new(crs_G_t, crs_H, multiply(R, int(k)), r_t)
+  cm_U = GroupCommitment.new(crs_G_u, crs_H, multiply(S, int(k)), r_u)
+
+  proof = SameScalarProof.new(
+    crs_G_t=crs_G_t,
+    crs_G_u=crs_G_u,
+    crs_H=crs_H,
+    R=R,
+    S=S,
+    cm_T=cm_T,
+    cm_U=cm_U,
+    k=k,
+    r_t=r_t,
+    r_u=r_u,
+    transcript=transcript_prover
+  )
+
+  print("proof", proof)
+
+  transcript_verifier = CurdleproofsTranscript()
+  (res, err) = proof.verify(
+    crs_G_t=crs_G_t,
+    crs_G_u=crs_G_u,
+    crs_H=crs_H,
+    R=R,
+    S=S,
+    cm_T=cm_T,
+    cm_U=cm_U,
+    transcript=transcript_verifier
+  )
+  print("res", res)
+  print("err", err)
+
+test_same_scalar_arg()
