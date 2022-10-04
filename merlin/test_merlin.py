9958d5590807656b8e37a38f5301f182edca125b
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2022-10-03 22:47:23 -0700

    working merlin transcript

--- merlin/test_merlin.py
+++ merlin/test_merlin.py
@@ -1,4 +1,5 @@
 
+from transcript import MerlinTranscript
 from strobe import Strobe128
 
 
@@ -24,3 +25,10 @@ def test_strobe_conformance():
 
   print("PRF: ", prf.hex())
   assert prf.hex() == "07e45cce8078cee259e3e375bb85d75610e2d1e1201c5f645045a194edd49ff8"
+
+def test_merlin():
+  transcript = MerlinTranscript(b"test protocol")
+  transcript.append_message(b"some label", b"some data")
+  challenge = transcript.challenge_bytes(b"challenge", 32)
+  print("challenge", challenge.hex())
+  assert challenge.hex() == "d5a21972d0d5fe320c0d263fac7fffb8145aa640af6e9bca177c03c7efcf0615"
