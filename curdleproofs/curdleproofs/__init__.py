805d06785b6ff35fde7148762277dd1ae678beeb
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2022-10-23 22:46:12 -0700

    expose more stuff externally for testing

--- curdleproofs/curdleproofs/__init__.py
+++ curdleproofs/curdleproofs/__init__.py
@@ -1,1 +1,9 @@
 from .whisk_interface import IsValidWhiskShuffleProof, IsValidWhiskOpeningProof
+from .opening import TrackerOpeningProof
+from .crs import CurdleproofsCrs
+from .curdleproofs import (
+    N_BLINDERS,
+    CurdleProofsProof,
+    VerifierInput,
+    shuffle_permute_and_commit_input,
+)
